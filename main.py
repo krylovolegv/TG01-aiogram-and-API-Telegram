51835662bb60a9b92dd51f353a06da49c5c7673c
krylovolegv <krylovolegv@yandex.ru>
2024-07-12 15:54:23 +0200

    add message.command

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
+
+import random
 
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+@dp.message(Command('photo'))
+async def photo(message: Message):
+    list = ['https://avatanplus.com/files/resources/original/58cc4caaaf88815ade0b7adf.png', 'https://i.ebayimg.com/images/g/x7EAAOSwGzxdbG~j/s-l500.jpg', 'https://avatars.mds.yandex.net/i?id=818f796dfb4c6612baccd142f4664084b19963f1-2417823-images-thumbs&n=13']
+    rand_photo = random.choice(list)
+    await message.answer_photo(photo=rand_photo, caption='Это супер крутая картинка')
+@dp.message(F.photo)
+async def react_photo(message: Message):
+    list = ['Ого, какая фотка!', 'Непонятно, что это такое', 'Не отправляй мне такое больше']
+    rand_answ = random.choice(list)
+    await message.answer(rand_answ)
+@dp.message(F. text == "что такое ИИ?")
+async def aitext(message: Message):
+    await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
+
+@dp.message(Command('help'))
+async def help(message: Message):
+    await message.answer("Этот бот умеет выполнять команды: \n /start \n /help")
 
 @dp.message(Command("start"))
 async def start(message: Message):
