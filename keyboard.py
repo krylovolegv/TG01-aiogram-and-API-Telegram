fd2ed78cfe1c364a15cbd1fcd696883ffda67aa9
krylovolegv <krylovolegv@yandex.ru>
2024-07-17 10:49:31 +0200

    recraft keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.types import KeyboardButton
 
 main = ReplyKeyboardMarkup(keyboard=[
    [KeyboardButton(text="Тестовая кнопка 1")],
@@ -7,18 +8,24 @@ main = ReplyKeyboardMarkup(keyboard=[
 ], resize_keyboard=True)
 
 inline_keyboard_test = InlineKeyboardMarkup(inline_keyboard=[
-   [InlineKeyboardButton(text="Видео", url='https://www.youtube.com/watch?v=AWfMoEiZX0c&t=16s')]
+   [InlineKeyboardButton(text="Каталог", callback_data='catalog')],
+   [InlineKeyboardButton(text="Новости", callback_data='news')],
+   [InlineKeyboardButton(text="Профиль", callback_data='person')]
 ])
 
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
-from aiogram.types import KeyboardButton
 
 # Список с четырьмя кнопками
 test = ["кнопка 1", "кнопка 2", "кнопка 3", "кнопка 4"]
 
 # Асинхронная функция для создания клавиатуры
+# async def test_keyboard():
+#     keyboard = ReplyKeyboardBuilder()
+#     for key in test:
+#         keyboard.add(KeyboardButton(text=key))
+#     return keyboard.adjust(2).as_markup()
+
 async def test_keyboard():
-    keyboard = ReplyKeyboardBuilder()
-    for key in test:
-        keyboard.add(KeyboardButton(text=key))
-    return keyboard.adjust(2).as_markup()
+   keyboard = InlineKeyboardBuilder()
+   for key in test:
+       keyboard.add(InlineKeyboardButton(text=key, url='https://www.youtube.com/watch?v=HfaIcB4Ogxk'))
+   return keyboard.adjust(2).as_markup()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import requests
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile, CallbackQuery
 import random
 from config import TOKEN
 from datetime import datetime, timedelta
@@ -130,11 +130,21 @@ async def react_photo(message: Message):
     await message.answer(f"Фото сохранено как {file_name}")
 
 
-@dp.message(F.text == "что такое ИИ?")
-async def aitext(message: Message):
-    await message.answer(
-        'Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
+# @dp.message(F.text == "что такое ИИ?")
+# async def aitext(message: Message):
+#     await message.answer(
+#         'Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
 
+# @dp.callback_query(F.data == 'news')
+# async def news(callback: CallbackQuery):
+#    await callback.answer("Новости подгружаются", show_alert=True)
+#    await callback.message.answer('Вот свежие новости!')
+
+# переход на другие кнопки
+@dp.callback_query(F.data == 'news')
+async def news(callback: CallbackQuery):
+   await callback.answer("Новости подгружаются", show_alert=True)
+   await callback.message.edit_text('Вот свежие новости!', reply_markup=await kb.test_keyboard())
 
 @dp.message(Command('help'))
 async def help(message: Message):
@@ -144,9 +154,13 @@ async def help(message: Message):
 @dp.message(Command("start"))
 async def start(message: Message):
     # await message.answer(f'Приветики, {message.from_user.full_name}', reply_markup=kb.main)
-    # await message.answer(f'Приветики, {message.from_user.full_name}', reply_markup=kb.inline_keyboard_test)
-    await message.answer(f'Приветики, {message.from_user.first_name}',
-                         reply_markup=await kb.test_keyboard())
+    await message.answer(f'Приветики, {message.from_user.full_name}', reply_markup=kb.inline_keyboard_test)
+    # await message.answer(f'Приветики, {message.from_user.first_name}',
+    #                      reply_markup=await kb.test_keyboard())
+
+@dp.message(F.text == "Тестовая кнопка 1")
+async def test_button(message: Message):
+   await message.answer('Обработка нажатия на reply кнопку')
 
 # @dp.message()
 # async def start(message: Message):
